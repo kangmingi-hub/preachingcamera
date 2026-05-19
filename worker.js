d80ae9dac900d0276f990efd1b9fa7708da73747
kangmingi-hub <rkdalsrlyt@gmail.com>
2026-05-20 07:11:35 +0900

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,14 +1,7 @@
-// ⚠️ 이 파일은 build.js가 자동 생성합니다
-// 하지만 Cloudflare CI에서 생성된 파일은 깃헙에 안 올라가므로
-// build.js 없이 직접 src 파일들을 import 합니다
-
 import { HTML } from './src/html.js';
 import { LOGIN_HTML } from './src/login.js';
 import { CSS } from './src/css.js';
 import APP_JS from './src/app.js';
-
-// collection.html을 직접 텍스트로 import
-import COLLECTION_HTML from './src/collection.html';
 
 const FINAL_HTML = HTML
   .replace('${CSS}', CSS)
